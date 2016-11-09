1a2db90a3780f4e89e933a62d0916c508f4fada8
fanrangjun <fanrangjun880506@yeah.net>
2016-11-09 09:26:40 +0800

    done lab4q4

--- kern/trap.c
+++ kern/trap.c
@@ -9,6 +9,29 @@
 #include <kern/env.h>
 #include <kern/syscall.h>
 
+// declaring the handlers used in entry_trap.S
+    void int0 ();
+    void int1 ();
+    void int2 ();
+    void int3 ();
+    void int4 ();
+    void int5 ();
+    void int6 ();
+    void int7 ();
+    void int8 ();
+    void int9 ();
+    void int10 ();
+    void int11 ();
+    void int12 ();
+    void int13 ();
+    void int14 ();
+    void int15 ();
+    void int16 ();
+    void int17 ();
+    void int18 ();
+    void int19 ();
+    void int48 ();
+
 static struct Taskstate ts;
 
 /* For debugging, so print_trapframe can distinguish between printing
@@ -65,7 +88,28 @@ trap_init(void)
 	extern struct Segdesc gdt[];
 
 	// LAB 3: Your code here.
+	SETGATE(idt[0], 0, GD_KT, int0, 0);
+    SETGATE(idt[1], 0, GD_KT, int1, 0);
+    SETGATE(idt[2], 0, GD_KT, int2, 0);
+    SETGATE(idt[3], 0, GD_KT, int3, 3);  // breakpoint
 
+    SETGATE(idt[4], 0, GD_KT, int4, 0);
+    SETGATE(idt[5], 0, GD_KT, int5, 0);
+    SETGATE(idt[6], 0, GD_KT, int6, 0);
+    SETGATE(idt[7], 0, GD_KT, int7, 0);
+    SETGATE(idt[8], 0, GD_KT, int8, 0);
+
+    SETGATE(idt[10], 0, GD_KT, int10, 0);
+    SETGATE(idt[11], 0, GD_KT, int11, 0);
+    SETGATE(idt[12], 0, GD_KT, int12, 0);
+    SETGATE(idt[13], 0, GD_KT, int13, 0);
+    SETGATE(idt[14], 0, GD_KT, int14, 0);
+
+    SETGATE(idt[16], 0, GD_KT, int16, 0);
+    SETGATE(idt[17], 0, GD_KT, int17, 0);
+    SETGATE(idt[18], 0, GD_KT, int18, 0);
+    SETGATE(idt[19], 0, GD_KT, int19, 0);
+    SETGATE(idt[48], 0, GD_KT, int48, 3);   // system call
 	// Per-CPU setup 
 	trap_init_percpu();
 }
@@ -143,6 +187,31 @@ trap_dispatch(struct Trapframe *tf)
 {
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
+	if (tf->tf_trapno == T_PGFLT) 
+	{
+		page_fault_handler(tf);
+        return;
+	}
+
+	 if (tf->tf_trapno == T_BRKPT) 
+	{
+		monitor(tf);
+        return;
+	}
+	if (tf->tf_trapno == T_SYSCALL) 
+	{	
+		tf->tf_regs.reg_eax =   syscall(tf->tf_regs.reg_eax,    tf->tf_regs.reg_edx,    tf->tf_regs.reg_ecx,    tf->tf_regs.reg_ebx,    tf->tf_regs.reg_edi,    tf->tf_regs.reg_esi);
+        return; 
+	}
+
+	// Handle spurious interrupts
+	// The hardware sometimes raises these because of noise on the
+	// IRQ line or other reasons. We don't care.
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_SPURIOUS) {
+		cprintf("Spurious interrupt on irq 7\n");
+		print_trapframe(tf);
+		return;
+	}
 
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
@@ -204,6 +273,9 @@ page_fault_handler(struct Trapframe *tf)
 	// Handle kernel-mode page faults.
 
 	// LAB 3: Your code here.
+
+	if ((tf->tf_cs&3) == 0)
+        panic("Kernel page fault!");
 
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
